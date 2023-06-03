3e0ae187013dfcc644043325ac1ff0fb5e0dda9a
charafmrah <charaf4charaf@gmail.com>
2023-06-03 18:58:26 +0300

    amplify config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
 // routes
 import Router from './routes';
 // theme
@@ -9,6 +13,8 @@ import { StyledChart } from './components/chart';
 import ScrollToTop from './components/scroll-to-top';
 
 // ----------------------------------------------------------------------
+
+Amplify.configure(awsconfig);
 
 export default function App() {
   return (
